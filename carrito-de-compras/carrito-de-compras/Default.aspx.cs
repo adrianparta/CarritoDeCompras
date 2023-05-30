645ef537c245f2cca529987afb43196f5b1b1e95
adrian <adrianpartarrieu99@gmail.com>
2023-05-30 01:43:44 -0300

    masterpage y muestra basica de las tarjetas

--- carrito-de-compras/carrito-de-compras/Default.aspx.cs
+++ carrito-de-compras/carrito-de-compras/Default.aspx.cs
@@ -4,14 +4,19 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using Business;
+using Domain;
 
 namespace carrito_de_compras
 {
     public partial class WebForm1 : System.Web.UI.Page
     {
+        public List<Item> ListItem { get; set; }
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            ListItem = ItemBusiness.List();
+            repeaterDefault.DataSource = ListItem;
+            repeaterDefault.DataBind();
         }
     }
 }
